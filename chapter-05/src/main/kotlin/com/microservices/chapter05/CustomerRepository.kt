f094e81a9a42653b746900c7ba086de43fdf8203
Antop <antop@naver.com>
2019-05-16 00:38:41 +0900

    kotlin-msa-6 스프링 데이터 사용 kotlin-msa-7 리액티브 리포지토리

---
+++ chapter-05/src/main/kotlin/com/microservices/chapter05/CustomerRepository.kt
@@ -0,0 +1,27 @@
+package com.microservices.chapter05
+
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate
+import org.springframework.stereotype.Repository
+import reactor.core.publisher.Mono
+import reactor.core.publisher.toMono
+import javax.annotation.PostConstruct
+
+@Repository
+class CustomerRepository(private val template: ReactiveMongoTemplate) {
+    companion object {
+        val initialCustomers = listOf(
+                Customer(1, "Kotlin"),
+                Customer(2, "Spring"),
+                Customer(3, "Microservice", Telephone("+44", "7123456789"))
+        )
+    }
+
+    @PostConstruct
+    fun initializeRepository() =
+            initialCustomers
+                    .map(Customer::toMono)
+                    .map(this::create)
+                    .map(Mono<Customer>::subscribe)
+
+    fun create(customer: Mono<Customer>): Mono<Customer> = template.save(customer)
+}
