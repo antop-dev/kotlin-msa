45e207a77228e9b20125f631a11ffcdede4377c6
Antop <antop@naver.com>
2019-06-13 23:38:44 +0900

    kotlin-msa-30 모의 빈 사용하기

--- chapter-09/src/test/kotlin/com/microservices/chapter09/CustomerControllerTest.kt
+++ chapter-09/src/test/kotlin/com/microservices/chapter09/CustomerControllerTest.kt
@@ -2,9 +2,15 @@ package com.microservices.chapter09
 
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.BDDMockito
+import org.mockito.BDDMockito.given
+import org.mockito.BDDMockito.then
+import org.mockito.Mockito.reset
+import org.mockito.Mockito.times
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
@@ -18,6 +24,8 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 class CustomerControllerTest {
     @Autowired
     lateinit var mockMvc: MockMvc
+    @MockBean
+    lateinit var customerService: CustomerService
 
     @Test
     fun `mock mvc should be configured`() {
@@ -26,24 +34,43 @@ class CustomerControllerTest {
 
     @Test
     fun `we should GET a customer by id`() {
+        given(customerService.getCustomer(1))
+                .willReturn(Customer(1, "mock customer"))
+
         mockMvc.perform(get("/customer/1"))
                 .andExpect(status().isOk)
                 .andExpect(jsonPath("\$.id").value(1))
-                .andExpect(jsonPath("\$.name").value("Kotlin"))
+                .andExpect(jsonPath("\$.name").value("mock customer"))
                 .andDo(print())
+
+        then(customerService).should(times(1)).getCustomer(1)
+        then(customerService).shouldHaveNoMoreInteractions()
+
+        reset(customerService)
     }
 
     @Test
     fun `we should GET a list of customers`() {
+        BDDMockito.given(customerService.getAllCustomers())
+                .willReturn(listOf(
+                        Customer(1, "test"),
+                        Customer(2, "mocks")
+                ))
+
         mockMvc.perform(get("/customers"))
                 .andExpect(status().isOk)
                 .andExpect(jsonPath("\$[0].id").value(1))
-                .andExpect(jsonPath("\$[0].name").value("Kotlin"))
+                .andExpect(jsonPath("\$[0].name").value("test"))
                 .andExpect(jsonPath("\$[1].id").value(2))
-                .andExpect(jsonPath("\$[1].name").value("Spring"))
-                .andExpect(jsonPath("\$[2].id").value(3))
-                .andExpect(jsonPath("\$[2].name").value("Microservice"))
+                .andExpect(jsonPath("\$[1].name").value("mocks"))
                 .andDo(print())
+
+        // customerService.getAllCustomers() 를 한번 호출 했다.
+        then(customerService).should(times(1)).getAllCustomers()
+        // 더 이상 아무 액션도 없었다.
+        then(customerService).shouldHaveNoMoreInteractions()
+
+        reset(customerService)
     }
 }
 
